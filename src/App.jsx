1d8d4d82a7ae15aabfa69b7b93b6b7520d936ecc
Ritchie61 <124876391+Ritchie61@users.noreply.github.com>
2025-12-09 23:38:06 +1000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 font-sans">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
